78b20c30402a2cf0839c4a99afefa4618906f3a3
Matthias Schmidt <schmidt.matthias93@outlook.com>
2019-03-15 19:25:50 +0100

    Add interface and simulator class

--- xvc_helper.py
+++ xvc_helper.py
@@ -1,12 +1,14 @@
 from enum import Enum
+import logging
 from typing import List, Tuple
+from abc import abstractmethod, ABCMeta
 
 from miio import Vacuum
 
 from xvc_util import XVCListable
 
 
-class XVCHelper(object):
+class XVCHelperBase(metaclass=ABCMeta):
     """
     Helper class to abstract and simplify vacuum methods.
     """
@@ -21,7 +23,109 @@ class XVCHelper(object):
         Max = 100
         Mob = 105
 
-    __RESPONSE_SUCCEEDED = ['ok']
+    RESPONSE_SUCCEEDED = ['ok']
+
+    @abstractmethod
+    def status(self) -> Tuple[bool, str]:
+        """
+        Gets current status.
+
+        :return: True on success, otherwise False.
+        :return:
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def pause(self) -> bool:
+        """
+        Pause vacuum cleaner.
+
+        :return: True on success, otherwise False.
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def start_zone_cleaning(self, zones: List[XVCListable]) -> bool:
+        """
+        Start the zone cleanup.
+
+        :param zones: Different zones to clean.
+        :return: True on success, otherwise False.
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def set_fan_level(self, fan_level: FanLevel) -> bool:
+        """
+        Sets the fan level.
+
+        :param fan_level: New fan level.
+        :return: True on success, otherwise False.
+        """
+        raise NotImplementedError()
+
+
+class XVCHelperSimulator(XVCHelperBase):
+    """
+    Helper class to abstract and simplify vacuum methods.
+    """
+
+    def __init__(self, ip: str, token: str) -> None:
+        """
+        Initialize a object of class XVCHelper.
+
+        :param ip: IP address of the vacuum cleaner.
+        :param token: Token of the vacuum cleaner.
+        """
+        self.__ip = ip
+        self.__token = token
+
+    def status(self) -> Tuple[bool, str]:
+        """
+        Gets current status.
+
+        :return: True on success, otherwise False.
+        :return:
+        """
+        logging.info('XVCHelperSimulator: status()')
+        return True, 'State: Simulation'
+
+    def pause(self) -> bool:
+        """
+        Pause vacuum cleaner.
+
+        :return: True on success, otherwise False.
+        """
+        logging.info('XVCHelperSimulator: pause()')
+        return True
+
+    def start_zone_cleaning(self, zones: List[XVCListable]) -> bool:
+        """
+        Start the zone cleanup.
+
+        :param zones: Different zones to clean.
+        :return: True on success, otherwise False.
+        """
+        logging.info('XVCHelperSimulator: start_zone_cleaning()')
+        for zone in zones:
+            logging.info('XVCHelperSimulator: {}'.format(zone))
+        return True
+
+    def set_fan_level(self, fan_level: XVCHelperBase.FanLevel) -> bool:
+        """
+        Sets the fan level.
+
+        :param fan_level: New fan level.
+        :return: True on success, otherwise False.
+        """
+        logging.info('XVCHelperSimulator: set_fan_level()')
+        return True
+
+
+class XVCHelper(XVCHelperBase):
+    """
+    Helper class to abstract and simplify vacuum methods.
+    """
 
     def __init__(self, ip: str, token: str) -> None:
         """
@@ -49,7 +153,7 @@ class XVCHelper(object):
         :return: True on success, otherwise False.
         """
         result = self.__vacuum.pause()
-        return result == XVCHelper.__RESPONSE_SUCCEEDED
+        return result == XVCHelper.RESPONSE_SUCCEEDED
 
     def start_zone_cleaning(self, zones: List[XVCListable]) -> bool:
         """
@@ -61,9 +165,9 @@ class XVCHelper(object):
         zones_list = [zone.get_list() for zone in zones]
         result = self.__vacuum.zoned_clean(zones_list)
         # self.pause()  # for debugging
-        return result == XVCHelper.__RESPONSE_SUCCEEDED
+        return result == XVCHelper.RESPONSE_SUCCEEDED
 
-    def set_fan_level(self, fan_level: FanLevel) -> bool:
+    def set_fan_level(self, fan_level: XVCHelperBase.FanLevel) -> bool:
         """
         Sets the fan level.
 
@@ -71,4 +175,4 @@ class XVCHelper(object):
         :return: True on success, otherwise False.
         """
         result = self.__vacuum.set_fan_speed(fan_level.value)
-        return result == XVCHelper.__RESPONSE_SUCCEEDED
+        return result == XVCHelper.RESPONSE_SUCCEEDED
