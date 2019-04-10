29b2a1ceb048776f2c71e98b1d74a79c857152e8
Matthias Schmidt <schmidt.matthias93@outlook.com>
2019-04-10 19:45:11 +0200

    change logging format

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ file_logging.setLevel(logging.INFO)
 
 logging.basicConfig(level=logging.NOTSET,
                     handlers=[console_logging, file_logging],
-                    format='%(asctime)23s - %(levelname)6s - %(funcName)25s - %(message)s'
+                    format='%(asctime)23s - %(levelname)8s - %(funcName)25s - %(message)s'
                     )
 
 
